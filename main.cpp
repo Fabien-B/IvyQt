81b8d9b7f436ded1f1cf140ae973cecc1cc97abc
Fabien-B <fabien.bonneval@gmail.com>
2021-04-20 15:58:59 +0200

    Update example.

--- main.cpp
+++ main.cpp
@@ -1,56 +1,74 @@
-#include <QCoreApplication>
+#include <QApplication>
+#include <QtWidgets>
 #include <QDebug>
 #include "ivyqt.h"
 #include <QTimer>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
     //auto localhost = "127.255.255.255";
 
+    auto window = new QMainWindow();
+    auto central_widget = new QWidget(window);
+    window->setCentralWidget(central_widget);
+    auto lay = new QHBoxLayout(central_widget);
+    auto label = new QLabel("label", window);
+    lay->addWidget(label);
+    auto b = new QPushButton("del label", window);
+    lay->addWidget(b);
 
-    auto ivyqt = new IvyQt("plop", "plop Ready");
-    ivyqt->setFlushTimeout(200);
-    ivyqt->start("192.168.1.255", 2010);
-    ivyqt->bindMessage("Hello (.*)", [=](Peer* sender, QStringList params){
-       qDebug() << "Hello with " << params[0] << " from " << sender->name();
+    QObject::connect(b, &QPushButton::clicked, [=]() {
+        lay->removeWidget(label);
+        label->deleteLater();
     });
 
-    ivyqt->bindMessage("^^Yo (.*)", [=](Peer* sender, QStringList params){
+    auto ivyqt = new IvyQt("plop", "plop Ready");
+    ivyqt->start("192.168.1.255", 2010);
+
+    ivyqt->bindMessage("^Yo (.*)", [=](Peer* sender, QStringList params){
        qDebug() << "Yo with " << params[0] << " from " << sender->name();
     });
 
-    QObject::connect(ivyqt, &IvyQt::peerReady, [=](Peer* peer){
-        qDebug() << peer->name() << "connected";
+    ivyqt->bindMessage("Hello (.*)", label, [=](Peer* sender, QStringList params) {
+       qDebug() << "Hello with " << params[0] << " from " << sender->name();
+       label->setText(params[0]);
     });
 
-    QObject::connect(ivyqt, &IvyQt::peerDied, [=](QString name){
-        qDebug() << name << "died";
-    });
 
-    QObject::connect(ivyqt, &IvyQt::directMessage, [=](Peer* peer, int ident, QString params){
-        qDebug() << "direct message from " << peer->name() << " id:" << ident << " params:" << params;
-    });
 
-    QObject::connect(ivyqt, &IvyQt::stopped, [=](){
-        qDebug() << "restart on different port";
-        ivyqt->start("192.168.1.255", 2011);
-    });
+//    QObject::connect(ivyqt, &IvyQt::peerReady, [=](Peer* peer){
+//        qDebug() << peer->name() << "connected";
+//    });
 
-    QObject::connect(ivyqt, &IvyQt::quitRequest, [=](Peer* peer){
-        auto bindings = peer->getBindings();
-        qDebug() << bindings;
-    });
+//    QObject::connect(ivyqt, &IvyQt::peerDied, [=](QString name){
+//        qDebug() << name << "died";
+//    });
 
-    QStringList list = {"Hello you", "Yo toi", "12 abc"};
-    auto timer = new QTimer();
-    timer->setInterval(2000);
-    QObject::connect(timer, &QTimer::timeout, [=]() {
-        static int i = 0;
-        ivyqt->send(list[i]);
-        i = (i+1) % list.length();
-    });
-    timer->start();
+//    QObject::connect(ivyqt, &IvyQt::directMessage, [=](Peer* peer, int ident, QString params){
+//        qDebug() << "direct message from " << peer->name() << " id:" << ident << " params:" << params;
+//    });
 
+//    QObject::connect(ivyqt, &IvyQt::stopped, [=](){
+//        qDebug() << "restart on different port";
+//        ivyqt->start("192.168.1.255", 2011);
+//    });
+
+//    QObject::connect(ivyqt, &IvyQt::quitRequest, [=](Peer* peer){
+//        auto bindings = peer->getBindings();
+//        qDebug() << bindings;
+//    });
+
+//    QStringList list = {"Hello you", "Yo toi", "12 abc"};
+//    auto timer = new QTimer();
+//    timer->setInterval(2000);
+//    QObject::connect(timer, &QTimer::timeout, [=]() {
+//        static int i = 0;
+//        ivyqt->send(list[i]);
+//        i = (i+1) % list.length();
+//    });
+//    timer->start();
+
+    window->show();
     return a.exec();
 }
